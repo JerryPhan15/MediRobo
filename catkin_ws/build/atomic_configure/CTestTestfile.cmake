b0dfd093ec6dfa5480db6a4d1c232948d64125a1
JerryPhan15 <phuy1380@gmail.com>
2025-12-19 23:49:55 +0700

    Delete catkin_ws/build/atomic_configure directory

--- catkin_ws/build/atomic_configure/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sun/catkin_ws/src/cartgrapher_navigation
-# Build directory: /home/sun/catkin_ws/build/cartgrapher_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
